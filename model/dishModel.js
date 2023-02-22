16508b13b167cde26313127e69b47dac1da1ccf8
Santosh Kumar <santosh.283143@gmail.com>
2023-02-22 14:55:18 +0530

    updated

--- model/dishModel.js
+++ model/dishModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const assert = require('assert')
+
 const connect1 = mongoose.createConnection(process.env.DISH_MONGO_URL || 'mongodb+srv://santosh283143:santosh1437@cluster0.nzq3jw9.mongodb.net/AdaptReady', (err) => {
     if (err) assert.deepStrictEqual(err, null)
     console.log(`dish database connected successfully`);
--- model/userModel.js
+++ model/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const assert = require('assert')
+
 const connect2 = mongoose.createConnection(process.env.USER_MONGO_URL || 'mongodb+srv://santosh283143:santosh1437@cluster0.nzq3jw9.mongodb.net/cmsbd?retryWrites=true&w=majority&authSource=admin&ssl=true', (err) => {
     if (err) assert.deepStrictEqual(err, null)
     console.log(`user database connected successfully`);
